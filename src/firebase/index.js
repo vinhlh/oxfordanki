97ba4dbab72ab7df6e05c41ba17282c353e24ab6
Vinh Le <vinh.ninja@icloud.com>
2021-02-15 17:51:21 +0800

    Refer from env vars

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,15 +2,26 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
+const {
+  API_KEY: apiKey,
+  AUTH_DOMAIN: authDomain,
+  DATABASE_URL: databaseURL,
+  PROJECT_ID: projectId,
+  STORAGE_BUCKET: storageBucket,
+  MESSAGING_SENDER_ID: messagingSenderId,
+  APP_ID: appId,
+  MEASUREMENT_ID: measurementId,
+} = process.env
+
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: ''
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
 }
 
 firebase.initializeApp(firebaseConfig)
